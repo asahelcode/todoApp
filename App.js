50f0967bf36301af1ec9f922cd7b278add659211
asahelcode <hoyx0101@gmail.com>
2023-01-22 00:01:42 +0100

    Add React Native Navigation

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+import Header from './components/Header';
+import Add from './components/Add';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView>
-        
-      </KeyboardAvoidingView>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <Header />
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Add" component={Add} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 35,
   },
 });
